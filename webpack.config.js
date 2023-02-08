57d5397255b04b7050b34b78a470a6099744e39d
Tang <2714148252@qq.com>
2023-02-08 23:42:23 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
+    devtool:'inline-source-map',
+   devServer: {
+    contentBase: "./dist"
+  },
     entry: './src/index.js',
     output: {
         filename: '[index].[contenthash].js',
